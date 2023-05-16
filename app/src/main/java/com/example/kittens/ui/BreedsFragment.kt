02db788724959c7cc341676289c50b075f8acac8
romanvit <romanvit13@gmail.com>
2023-05-17 00:23:51 +0300

    Add a view model for the breeds fragment

--- app/src/main/java/com/example/kittens/ui/BreedsFragment.kt
+++ app/src/main/java/com/example/kittens/ui/BreedsFragment.kt
@@ -1,25 +1,17 @@
 package com.example.kittens.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.room.ColumnInfo
-import com.example.kittens.App
+import androidx.lifecycle.ViewModelProvider
 import com.example.kittens.databinding.FragmentBreedsBinding
-import com.example.kittens.models.Breed
-import com.example.kittens.models.Weight
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class BreedsFragment : Fragment() {
     private var _binding: FragmentBreedsBinding? = null
     private val binding get() = _binding!!
-
-    private var breeds: List<Breed>? = null
+    private var viewModel: BreedsViewModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,73 +25,25 @@ class BreedsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.saveToDbBtn.setOnClickListener {
-            val backgroundThread = Thread {
-                insertBreedsToDb()
-            }
+        viewModel = ViewModelProvider(this)[BreedsViewModel::class.java]
 
-            backgroundThread.start()
+        binding.saveToDbBtn.setOnClickListener {
+            // to do
         }
 
-        getBreeds()
+        binding.loadFromDb.setOnClickListener {
+            viewModel?.loadBreedsFromDb()
+        }
+
+        viewModel?.obtainBreeds()
+
+        viewModel?.breeds?.observe(viewLifecycleOwner) {
+            binding.breedsInfoTv.text = it.toString()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun getBreeds() {
-        val call: Call<MutableList<Breed>>? = App.catService?.getAllBreeds()
-        call?.enqueue(object : Callback<MutableList<Breed>> {
-            override fun onResponse(
-                call: Call<MutableList<Breed>>,
-                response: Response<MutableList<Breed>>
-            ) {
-                val breedsList = response.body()
-                breeds = breedsList
-                val breedsString = StringBuilder()
-                if (breedsList != null) {
-                    for (breed in breedsList) {
-                        Log.d("BreedsFragment", "Breed: $breed")
-                        breedsString.append(breed).append("\n")
-                    }
-                }
-
-                binding.breedsInfoTv.text = breedsString.toString()
-            }
-
-            override fun onFailure(call: Call<MutableList<Breed>>, t: Throwable) {
-                t.message?.let {
-                    Log.e("BreedsFragment", it)
-                }
-            }
-        })
-    }
-
-    private fun insertBreedsToDb() {
-        val breedsList = breeds
-        if (breedsList == null) {
-            Log.d("BreedsFragment", "Breeds are null")
-            return
-        }
-
-        for (breed in breedsList) {
-            Log.d("BreedsFragment", "Breed: $breed")
-            breed.apply {
-                val breedDb = com.example.kittens.room.models.Breed(
-                    id,
-                    name,
-                    temperament,
-                    origin,
-                    countryCodes,
-                    countryCode,
-                    lifeSpan,
-                    "wikipediaUrl"
-                )
-
-                App.appDatabase?.breedDao()?.insertAll(breedDb)
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/example/kittens/ui/BreedsViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.kittens.ui
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kittens.App
+import com.example.kittens.models.Breed
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BreedsViewModel : ViewModel() {
+    val breeds = MutableLiveData<List<Breed>>()
+    private var breedsList: List<Breed>? = null
+
+    fun obtainBreeds() {
+        val call: Call<MutableList<Breed>>? = App.catService?.getAllBreeds()
+        call?.enqueue(object : Callback<MutableList<Breed>> {
+            override fun onResponse(
+                call: Call<MutableList<Breed>>,
+                response: Response<MutableList<Breed>>
+            ) {
+                val breedsList = response.body()
+                this@BreedsViewModel.breedsList = breedsList
+                breedsList?.let { breeds.postValue(it) }
+            }
+
+            override fun onFailure(call: Call<MutableList<Breed>>, t: Throwable) {
+                t.message?.let {
+                    Log.e("BreedsFragment", it)
+                }
+            }
+        })
+    }
+
+    fun loadBreedsFromDb() {
+        var breeds: List<com.example.kittens.room.models.Breed>? = null
+        val runnable = {
+            breeds = App.appDatabase?.breedDao()?.getAll()
+        }
+        Thread(runnable).start()
+    }
+}
